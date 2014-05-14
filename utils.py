370d84ed6b6ebccfbf33efcf77cefa52d19f30a3
Misty De Meo <mdemeo@artefactual.com>
2014-05-14 10:24:14 -0700

    check_valid_uuid: update docstring

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ def check_valid_uuid(uuid):
     """
     Checks to see if the passed string is a valid UUID.
 
+    The passed UUID must be a string; if it is not, a TypeError is raised.
+
     This uses the UUID() class constructor from the uuid module,
     which raises a ValueError if the passed string is not a valid UUID.
     """

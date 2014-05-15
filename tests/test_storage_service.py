0b68d0b24aef2194119d76e5ebd20bfe656633e5
Misty De Meo <mdemeo@artefactual.com>
2014-05-15 16:50:23 -0700

    Add storage_service tests
    These tests use py.test and VCR.
    
    VCR records interaction with external services, then substitutes the
    recorded interactions on subsequent test runs. Every test that needs
    to communicate with the storage service has a cassette committed in the
    repository, so these tests can be run without access to a functioning
    storage service instance.

---
+++ tests/test_storage_service.py
@@ -0,0 +1,96 @@
+import json
+import os
+import sys
+
+sys.path.append(os.path.normpath(os.path.join(__file__, '..', '..')))
+from fixity import storage_service
+
+import pytest
+import vcr
+
+
+STORAGE_SERVICE_URL = 'http://localhost:8000/'
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/single_aip.yaml')
+def test_get_single_aip():
+    aip_uuid = 'a7f2a05b-0fdf-42f1-a46c-4522a831cf17'
+    aip = storage_service.get_single_aip(aip_uuid, STORAGE_SERVICE_URL)
+    assert type(aip) == dict
+    assert aip['uuid'] == aip_uuid
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/all_aips.yaml')
+def test_get_all_aips():
+    aip_uuids = ('a7f2a05b-0fdf-42f1-a46c-4522a831cf17', 'c8ebb75e-6b7a-46dd-a360-91d3753d7b72')
+
+    aips = storage_service.get_all_aips(STORAGE_SERVICE_URL)
+    assert len(aips) == 2
+    for aip in aips:
+        assert type(aip) == dict
+        assert aip['uuid'] in aip_uuids
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/single_aip_404.yaml')
+def test_single_aip_raises_on_404():
+    with pytest.raises(storage_service.StorageServiceError) as ex:
+        storage_service.get_single_aip('ba31d9b8-5baa-4d62-839e-cf71497d4acf', STORAGE_SERVICE_URL)
+
+    assert "returned 404" in ex.value.message
+    assert ex.value.report is None
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/single_aip_500.yaml')
+def test_single_aip_raises_on_500():
+    with pytest.raises(storage_service.StorageServiceError) as ex:
+        storage_service.get_single_aip('a7f2a05b-0fdf-42f1-a46c-4522a831cf17', STORAGE_SERVICE_URL)
+
+    assert "internal error" in ex.value.message
+    assert ex.value.report is None
+
+
+def test_single_aip_raises_with_invalid_uuid():
+    with pytest.raises(storage_service.InvalidUUID):
+        storage_service.get_single_aip('foo', STORAGE_SERVICE_URL)
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/all_aips_500.yaml')
+def test_get_all_aips_raises_on_500():
+    with pytest.raises(storage_service.StorageServiceError) as ex:
+        storage_service.get_all_aips(STORAGE_SERVICE_URL)
+
+    assert "internal error" in ex.value.message
+    assert ex.value.report is None
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/fixity_success.yaml')
+def test_successful_fixity_scan():
+    success, report = storage_service.scan_aip('c8ebb75e-6b7a-46dd-a360-91d3753d7b72', STORAGE_SERVICE_URL)
+
+    assert success is True
+    assert report.success is True
+
+    parsed_report = json.loads(report.report)
+    for category in ('untracked', 'changed', 'missing'):
+        assert len(parsed_report['failures']['files'][category]) == 0
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/fixity_fail.yaml')
+def test_failed_fixity_scan():
+    success, report = storage_service.scan_aip('c8ebb75e-6b7a-46dd-a360-91d3753d7b72', STORAGE_SERVICE_URL)
+    assert success is False
+    assert report.success is False
+
+    parsed_report = json.loads(report.report)
+    assert len(parsed_report['failures']['files']['changed']) == 2
+    assert parsed_report['failures']['files']['changed'][0]['path'] == 'data/objects/oakland03.jp2'
+    for category in ('untracked', 'missing'):
+        assert len(parsed_report['failures']['files'][category]) == 0
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/fixity_500.yaml')
+def test_fixity_scan_raises_on_500():
+    with pytest.raises(storage_service.StorageServiceError) as ex:
+        storage_service.scan_aip('a7f2a05b-0fdf-42f1-a46c-4522a831cf17', STORAGE_SERVICE_URL)
+
+    assert "internal error" in ex.value.message

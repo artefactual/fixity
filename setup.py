205d1bf3a00fe0ea0eba51d48f9eb18587c78b7c
Misty De Meo <mdemeo@artefactual.com>
2014-05-15 10:16:12 -0700

    Rename main module

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="fixity",
     version="0.1",
     packages=["fixity"],
-    entry_points={"console_scripts": ["fixity = fixity.main:main"]}
+    entry_points={"console_scripts": ["fixity = fixity.fixity:main"]}
 )

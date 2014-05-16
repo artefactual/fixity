70da45236ad92aa2efadd07ffa88a718ce8ba4ec
Misty De Meo <mistydemeo@gmail.com>
2014-05-16 16:59:31 -0700

    Add test for POSTing fixity report

---
+++ tests/test_reporting.py
@@ -0,0 +1,33 @@
+from fixity.models import AIP, Report
+from fixity import reporting
+
+import vcr
+
+import os
+from datetime import datetime
+
+
+REPORT_URL = "http://localhost:8003/"
+
+
+def json_string(filename):
+    path = os.path.normpath(os.path.join(__file__, "..", "..", "fixtures", "json", filename))
+    with open(path) as jsonfile:
+        return jsonfile.read()
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/post_failed_report.yaml')
+def test_posting_report():
+    json_report = json_string("test_failed_report.json")
+    aip = AIP(
+        uuid="ed42aadc-d854-46c6-b455-cd384eef1618"
+    )
+    report = Report(
+        aip=aip,
+        begun=datetime.fromtimestamp(1400022946),
+        ended=datetime.fromtimestamp(1400023208),
+        success=False,
+        posted=False,
+        report=json_report
+    )
+    reporting.post_report(aip.uuid, report, REPORT_URL)

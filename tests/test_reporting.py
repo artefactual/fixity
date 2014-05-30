56a8470a9f3b53dfa6dc562df7952e2d5c717b0e
Misty De Meo <mistydemeo@gmail.com>
2014-05-30 14:41:00 -0700

    Report tests: test for 500 responses

--- tests/test_reporting.py
+++ tests/test_reporting.py
@@ -31,6 +31,14 @@ def test_posting_prescan_report_raises_on_invalid_uuid():
         reporting.post_pre_scan_report("foo", None, None)
 
 
+@vcr.use_cassette('fixtures/vcr_cassettes/post_prescan_report_500.yaml')
+def test_posting_prescan_report_raises_on_500():
+    with pytest.raises(reporting.ReportServiceException):
+        aip = "90cf0850-f5d2-4023-95e2-0e2b7a1e1b8e"
+        start_time = datetime.fromtimestamp(1400022946)
+        reporting.post_pre_scan_report(aip, start_time, REPORT_URL)
+
+
 @vcr.use_cassette('fixtures/vcr_cassettes/post_failed_report.yaml')
 def test_posting_success_report():
     json_report = json_string("test_failed_report.json")
@@ -51,3 +59,21 @@ def test_posting_success_report():
 def test_posting_success_report_raises_on_invalid_uuid():
     with pytest.raises(InvalidUUID):
         reporting.post_success_report("foo", None, None)
+
+
+@vcr.use_cassette('fixtures/vcr_cassettes/post_failed_report_500.yaml')
+def test_posting_success_report_raises_on_500():
+    with pytest.raises(reporting.ReportServiceException):
+        json_report = json_string("test_failed_report.json")
+        aip = AIP(
+            uuid="ed42aadc-d854-46c6-b455-cd384eef1618"
+        )
+        report = Report(
+            aip=aip,
+            begun=datetime.fromtimestamp(1400022946),
+            ended=datetime.fromtimestamp(1400023208),
+            success=False,
+            posted=False,
+            report=json_report
+        )
+        reporting.post_success_report(aip.uuid, report, REPORT_URL)

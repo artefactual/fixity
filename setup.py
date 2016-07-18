0f6210349316b70929034ff4ec9578352022789d
Holly Becker <hbecker@artefactual.com>
2016-07-18 10:09:12 -0700

    Setup.py: more metadata

--- setup.py
+++ setup.py
@@ -4,5 +4,15 @@ setup(
     name="fixity",
     version="0.2",
     packages=["fixity"],
-    entry_points={"console_scripts": ["fixity = fixity.fixity:main"]}
+    url = 'https://github.com/artefactual/fixity',
+    author = 'Artefactual Systems',
+    author_email = 'info@artefactual.com',
+    entry_points={"console_scripts": ["fixity = fixity.fixity:main"]},
+    classifiers = [
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )

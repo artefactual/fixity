04bb554f4b48204aec1e5db537ec04c0a08ca5fd
Holly Becker <hbecker@artefactual.com>
2016-07-18 10:03:45 -0700

    Increment version to 0.2 for auth updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="fixity",
-    version="0.1",
+    version="0.2",
     packages=["fixity"],
     entry_points={"console_scripts": ["fixity = fixity.fixity:main"]}
 )

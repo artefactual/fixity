8dec60853b27a15f66acb40f6f385c93c83d96ac
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2020-12-06 21:43:30 -0600

    Use Github Actions

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
 )

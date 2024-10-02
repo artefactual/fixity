1906a136272e65dcd8a86c830e7676fb8b844bb0
Dhwani Patel <dpatel@artefactual.com>
2024-10-02 15:49:29 -0600

    Add annotation for Base class

--- fixity/models.py
+++ fixity/models.py
@@ -1,4 +1,5 @@
 import os
+from typing import Type
 
 from sqlalchemy import Boolean
 from sqlalchemy import Column
@@ -7,7 +8,7 @@ from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy import create_engine
-from sqlalchemy.orm import backref
+from sqlalchemy.orm import backref, DeclarativeBase
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
@@ -16,7 +17,7 @@ db_path = os.path.join(os.path.dirname(__file__), "fixity.db")
 engine = create_engine(f"sqlite:///{db_path}", echo=False)
 
 Session = sessionmaker(bind=engine)
-Base = declarative_base()
+Base: Type[DeclarativeBase] = declarative_base()
 
 
 class AIP(Base):
